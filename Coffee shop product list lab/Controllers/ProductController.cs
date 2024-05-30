7c06cedf256d9e7ee8707e07d69c37b50e11db98
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-05-30 16:14:36 -0400

    first commit

---
+++ Coffee shop product list lab/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Coffee_shop_product_list_lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_shop_product_list_lab.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDbContext dbcontext = new ProductDbContext();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Product> products = dbcontext.Products.ToList();
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product result = dbcontext.Products.Find(id);
+            return View(result);
+        }
+    }
+}
---
+++ Coffee shop product list lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coffee_shop_product_list_lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
